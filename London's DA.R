a7c508b076bbbb8c77513342b871f119c7e8a580
Sumit Mishra <sumit.mishra0432@gmail.com>
2024-11-04 10:27:26 +0000

    Some DA

--- London's DA.R
+++ London's DA.R
@@ -5,6 +5,12 @@ test_benzene_08$V1 = NULL
 test_benzene_08 = test_benzene_08 %>%
   distinct()
 
-
+for (col_name in names(test_benzene_08)) {
+  if (class(test_benzene_08[[col_name]]) == 'character'){
+    print (paste0(col_name, " :: ", n_distinct(test_benzene_08[[col_name]])))
+  } else{
+    print (paste0(col_name, " :: ", n_distinct(test_benzene_08[[col_name]])))
+  }
+}
 test_benzene_10 = fread("Data/London Atmospheric Emissions Inventory/2. Emissions/Mobile/Road Transport/Excel/MajorRoads/Benzene_2010.csv")
 
