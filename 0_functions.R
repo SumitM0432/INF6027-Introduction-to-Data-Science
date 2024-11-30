efa217da7fa2b8212c199f98cde2bc328f1f1051
Sumit Mishra <sumit.mishra0432@gmail.com>
2024-11-30 09:58:03 +0000

    All

--- 0_functions.R
+++
@@ -1,9 +0,0 @@
-print_description = function(df) {
-  print (paste0("No. of Rows :: ", nrow(df)))
-  print (paste0("No. of Columns :: ", ncol(df)))
-  
-  # Column Names
-  ls = colnames(df)
-  
-  return (ls)
-}
