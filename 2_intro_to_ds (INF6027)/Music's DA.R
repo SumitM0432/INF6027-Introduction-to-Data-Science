11027cb6e4bf7242ee170fac1d6cff3e70ff5761
Sumit Mishra <sumit.mishra0432@gmail.com>
2024-12-03 11:50:23 +0000

    Working code

--- 2_intro_to_ds (INF6027)/Music's DA.R
+++ 2_intro_to_ds (INF6027)/Music's DA.R
@@ -6,20 +6,32 @@ if (interactive()) {
   }
 }
 
+# Getting the libraries and the required functions
 source("../0_config/0_libraries.R", local = TRUE)
 source("../0_config/0_functions.R", local = TRUE)
 
+# test code
+# con = dbConnect(SQLite(), '../1_datasets/MusicOSet/musicoset.db')
+
+# Read and execute the SQL script
+# sql_script <- readLines("../1_datasets/MusicOSet/musicoset.sql")
+# dbExecute(con, paste(sql_script, collapse = "\n"))
+
 con = dbConnect(RMySQL::MySQL(),
-                            dbname='musicoset',
-                            host='localhost',
-                            port=3306,
-                            user='root',
-                            password='qwe123@A@A')
+                host='localhost',
+                port=3306,
+                user='root',
+                password='qwe123@A@A')
 
 dbSendQuery(con, "SET GLOBAL local_infile = true;")
+dbSendQuery(con, "CREATE DATABASE IF NOT EXISTS musicoset;")
 
-result = dbSendQuery(con, "select * from musicoset.acoustic_features")
-acoustic_features = data.table(fetch(result, n = -1)) #n = -1 for all the rows
+result = dbSendQuery(con, "SELECT * FROM musicoset.acoustic_features")
+df_acoustic_features = data.table(fetch(result, n = -1)) #n = -1 for all the rows
+
+result = dbSendQuery(con, "SELECT * FROM musicoset.song_chart")
+df_song_chart = data.table(fetch(result, n = -1)) #n = -1 for all the rows
+
 
 list.files(path = "../1_datasets/MusicOSet/musicoset_metadata/", pattern = "*.csv")
 
