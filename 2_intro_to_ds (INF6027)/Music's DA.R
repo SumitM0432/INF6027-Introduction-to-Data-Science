dab6421742e005fbb4c11dceeea1a5f1a5754605
Sumit Mishra <sumit.mishra0432@gmail.com>
2024-12-05 15:21:17 +0000

    New Addition

--- 2_intro_to_ds (INF6027)/Music's DA.R
+++ 2_intro_to_ds (INF6027)/Music's DA.R
@@ -17,43 +17,37 @@ source("music_data_loading.R", local = TRUE)
 load("RData/Loaded_MusicData.RData")
 
 # 20406 -> 20405 # No NULLS in df_chart_songs and df_pop_songs
-df_meta_songs = df_meta_songs %>%
-  # Dropping the null values
-  drop_na()
-
 # No NULLS in df_pop_artists, df_chart_artists and df_meta_artists
 # No NULLS in df_acoustic_features
 # 744 NULLS in lyrics
 
-# Getting the Artists IDs
 df_meta_songs = df_meta_songs %>%
-  mutate(
-    artist_id_vectors := mapply(extract_artist, artists)
-  )
+  # Dropping the null values
+  drop_na()
 
-check_coverage = function(df1, df2, on_col) {
+df_meta_songs = df_meta_songs %>%
+  # Getting the Artists IDs
+  mutate(artist_id_vectors := mapply(extract_artist, artists)) %>%
+  # Counting the number of artist that has worked on that song
+  mutate(num_artist = str_count(artist_id_vectors, ",") + 1)
+
+temp = df_meta_songs %>%
+  mutate(temp_ids = artist_id_vectors) %>%
+  separate_rows(artist_id_vectors, sep = ",")
+
+get_artist_info = function (artist_ids_list, df_meta_artists = df_meta_artists) {
+  artist_ids_list = artist_ids_list[[1]]
   
-  # Selecting the only the ids for data frame 1 (on which we are gonna do the check)
-  df1 = df1 %>%
-    select(all_of(on_col)) %>%
-    distinct()
-  
-  # Temp flag for the data frame 2 with whom we are gonna check
-  df2 = df2 %>%
-    mutate(temp_flag = 1) %>%
-    select(all_of(c(on_col, 'temp_flag')))
-  
-  # ids not present count
-  not_present = left_join(df1, df2, by = c(on_col)) %>%
-    filter(is.na(temp_flag)) %>%
-    select(all_of(on_col))
-  
-  print (paste0("ids not present in the second tables :: count :: ", nrow(not_present)))
-  return (not_present)
+  artist_info = df_meta_artists %>%
+    filter(artist_id %in% artist_ids_list) %>%
+    mutate(followers = as.numeric(followers)) %>%
+    group_by() %>%
+    summarise(
+      total_followers = sum(followers),
+      avg_popularity = mean(popularity)
+    )
   
 }
-
-
 
 
 
