e3d3ebd90d0b16689411f6f9acce329049f8d1e8
Sumit Mishra <sumit.mishra0432@gmail.com>
2024-12-27 19:31:10 +0000

    Final run without lyrics and libraries updation

--- 0_config/0_libraries.R
+++ 0_config/0_libraries.R
@@ -1,41 +1,19 @@
-if (!requireNamespace("data.table")) install.packages("data.table")
-library("data.table")
-
-if (!requireNamespace("tidyverse")) install.packages("tidyverse")
-library("tidyverse")
-
-if (!requireNamespace("tm")) install.packages("tm")
-library("tm")
-
-if (!requireNamespace("SnowballC")) install.packages("SnowballC")
-library("SnowballC")
-
+# Data Ingestion
 if (!requireNamespace("RMySQL")) install.packages("RMySQL")
 library("RMySQL")
 
 if (!requireNamespace("DBI")) install.packages("DBI")
 library("DBI")
 
-if (!requireNamespace("odbc")) install.packages("odbc")
-library("odbc")
+# Preprocessing and Plotting
+if (!requireNamespace("data.table")) install.packages("data.table")
+library("data.table")
 
-if (!requireNamespace("fastDummies")) install.packages("fastDummies")
-library("fastDummies")
+if (!requireNamespace("tidyverse")) install.packages("tidyverse")
+library("tidyverse")
 
-if (!requireNamespace("tidymodels")) install.packages("tidymodels")
-library("tidymodels")
-
-if (!requireNamespace("glmnet")) install.packages("glmnet")
-library("glmnet")
-
-if (!requireNamespace("randomForest")) install.packages("randomForest")
-library("randomForest")
-
-if (!requireNamespace("xgboost")) install.packages("xgboost")
-library("xgboost")
-
-if (!requireNamespace("caret")) install.packages("caret")
-library("caret")
+if (!requireNamespace("tidytext")) install.packages("tidytext")
+library("tidytext")
 
 if (!requireNamespace("stopwords")) install.packages("stopwords")
 library("stopwords")
@@ -43,8 +21,9 @@ library("stopwords")
 if (!requireNamespace("syuzhet")) install.packages("syuzhet")
 library("syuzhet")
 
-if (!requireNamespace("tidytext")) install.packages("tidytext")
-library("tidytext")
+# For Model Training and Prediction
+if (!requireNamespace("caret")) install.packages("caret")
+library("caret")
 
-
-
+if (!requireNamespace("xgboost")) install.packages("xgboost")
+library("xgboost")
